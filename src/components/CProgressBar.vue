5a30bd39ddc5388eff26fe820e5177720ba5cc62
eminzzz <eminroth@gmail.com>
2021-09-16 02:12:40 +0300

    feat: CProgressBar.vue
    * created CProgressBar component
    * added support for dynamic styles

---
+++ src/components/CProgressBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="widget__progress"
+    :style="{ background: widgetColorsTheme.borderLineColor }"
+  >
+    <div
+      class="widget__progress-bar"
+      :style="{ background: widgetColorsTheme.ctaColor }"
+    ></div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "CProgressBar",
+  computed: {
+    ...mapGetters({
+      widgetColorsTheme: "widget/widgetColorsTheme",
+    }),
+  },
+};
+</script>
