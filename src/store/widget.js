7252c6167854c89e1402f61ac46f1724fcfa60e3
eminzzz <eminroth@gmail.com>
2021-09-16 23:45:43 +0300

    update: implemented new screens logic

--- src/store/widget.js
+++ src/store/widget.js
@@ -10,10 +10,24 @@ export default {
       errorColor: "#E65069",
     },
     currentScreen: "c-screen-1",
+    isMenu: false,
+    isMoreCoins: false,
   },
   mutations: {
     CHANGE_SCREEN(state, screen) {
       state.currentScreen = screen;
+    },
+    OPEN_MENU(state) {
+      state.isMenu = true;
+    },
+    CLOSE_MENU(state) {
+      state.isMenu = false;
+    },
+    OPEN_MORE_COINS(state) {
+      state.isMoreCoins = true;
+    },
+    CLOSE_MORE_COINS(state) {
+      state.isMoreCoins = false;
     },
   },
   getters: {
