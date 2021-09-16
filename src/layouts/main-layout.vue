cba03790be91692881c8334a167209ace7981ebd
eminzzz <eminroth@gmail.com>
2021-09-16 23:45:20 +0300

    feat: implemented main-layout

---
+++ src/layouts/main-layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <c-menu v-if="$store.state.widget.isMenu === true" />
+  <div v-else class="widget__wrapper">
+    <c-header @handleHeaderButtonClick="$store.commit('widget/OPEN_MENU')" />
+    <c-progress-bar />
+    <slot name="default"></slot>
+  </div>
+</template>
+
+<script>
+import CMenu from "@/views/CMenu";
+import CHeader from "@/components/CHeader";
+import CProgressBar from "@/components/CProgressBar";
+
+export default {
+  name: "main-layout",
+  components: { CProgressBar, CHeader, CMenu },
+};
+</script>
+
+<style scoped></style>
